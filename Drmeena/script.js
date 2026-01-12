4bbf84e91a07880bb916fb8594e8aea2b0fe2e94
Jaishree baskaran <138391080+Jaishree-baskaran@users.noreply.github.com>
2026-01-13 02:57:21 +0530

    Delete Drmeena directory

--- Drmeena/script.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {
-    var nav = document.querySelector('.navbar');
-    if (window.pageYOffset > 50) {
-        nav.style.boxShadow = "0 2px 10px rgba(0,0,0,0.1)";
-        nav.style.background = "#fdfdfd";
-    } else {
-        nav.style.boxShadow = "none";
-    }
-};
